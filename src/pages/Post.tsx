7004b74c35a920abc5e847b56ab6d0c192c97268
Marcos Corazza <mcs.corazza@gmail.com>
2026-04-25 18:21:10 -0300

    fix: mobile padding

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -86,16 +86,16 @@ export function PostPage() {
         </div>
       </aside>
 
-      <article className="prose prose-sky lg:prose-xl max-w-300 p-6">
+      <article className="prose prose-sky p-1 lg:prose-xl max-w-300 lg:p-6">
         <h1 className='text-4xl! text-cyan-50! drop-shadow-sm font-extrabold'>Estação #{currentPost.order.toString().padStart(2, '0')}</h1>
-        <div className='relative rounded-xl overflow-hidden'>
+        <div className='relative rounded-sm lg:rounded-xl overflow-hidden'>
           <img className='w-full object-cover' src={currentPost.coverImage} />
           <h2 className='absolute inset-0 bg-black/70 text-cyan-200! flex items-end justify-start drop-shadow-xl m-0! p-6 text-4xl! font-bold'>{currentPost.title}</h2>
         </div>
         <small className='text-center block tracking-wider text-sm text-cyan-200'>{currentPost.author} | {formatCustomDate(currentPost.date)} | {currentPost.tags}</small>
         <blockquote className='text-slate-300 text-base text-justify leading-relaxed'>{currentPost.summary}</blockquote>
 
-        <div className="prose prose-sm md:prose-base max-w-none font-sans p-6 rounded-xl bg-[#37474f]">
+        <div className="prose prose-sm md:prose-base max-w-none font-sans p-1 lg:p-6 rounded-sm lg:rounded-xl bg-[#37474f]">
           <ReactMarkdown
             rehypePlugins={[rehypeRaw]}
             remarkPlugins={[remarkGfm]}
